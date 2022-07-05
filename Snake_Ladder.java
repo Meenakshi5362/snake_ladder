cdd852e20ce4a5738b42df724f55590667301e25
unknown <annapureddy738@gmail.com>
2022-07-05 19:23:58 +0530

    Game played with 2 players and deciding who wins

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Snake_Ladder {
 	public static void main(String[] args) {
-		int player_1 = 0, dice,count1=0;
+		int player_1 = 0, dice, count1 = 0, player_2 = 0, count2 = 0;
 		String choice;
 		Random r = new Random();
 		while (true) {
@@ -16,26 +16,57 @@ public class Snake_Ladder {
 				if (player_1 + dice <= 100) {
 					player_1 = player_1 + dice;
 
-					}
-					else {
-						System.out.println("position exceeds 100");
-					}
-				
+				} else {
+					System.out.println("position exceeds 100");
+				}
+
 			} else if (choice == ("Snake")) {
 				player_1 = player_1 - dice;
 				if (player_1 < 0) {
 					player_1 = 0;
 				}
-				//System.out.println(player_1);
+				System.out.println(player_1);
 			} else {
 				System.out.println("No_Play condition occures");
 			}
-			if (player_1 >= 100) {
-				System.out.println("game ended");
+			if (player_1 == 100) {
+				System.out.println("player 1 wins ");
 				break;
 			}
-			count1++;System.out.println("Position of player after every dice role "+player_1);
+			count1++;
+			System.out.println("Position of player1 after every dice role " + player_1);
+			dice = 1 + r.nextInt(6);
+			System.out.println("Player_2 rolls a dice of " + dice);
+			// String option[] = { "No_Play", "Ladder", "Snake" };
+			choice = option[r.nextInt(3)];
+			System.out.println(choice);
+
+			if (choice == ("Ladder")) {
+				if (player_2 + dice <= 100) {
+					player_2 = player_2 + dice;
+
+				} else {
+					System.out.println("position exceeds 100");
+				}
+
+			} else if (choice == ("Snake")) {
+				player_2 = player_2 - dice;
+				if (player_2 < 0) {
+					player_2 = 0;
+				}
+				System.out.println(player_2);
+			} else {
+				System.out.println("No_Play condition occures");
+			}
+			if (player_2 == 100) {
+				System.out.println("player 2 wins");
+				break;
+			}
+			count2++;
+			System.out.println("Position of player2 after every dice role " + player_2);
+
 		}
-		System.out.println("No of times dice was played :"+count1);
+		System.out.println("No of times dice was played by player1 :" + count1);
+		System.out.println("No of times dice was played by player2 :" + count2);
 	}
 }
