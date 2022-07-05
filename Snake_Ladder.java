f5bd7a6119f7a9a620babfc0954ece5149ab59ba
unknown <annapureddy738@gmail.com>
2022-07-05 17:22:13 +0530

    Repating till the player reaches winning position 100

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -5,31 +5,32 @@ public class Snake_Ladder {
 		int player_1 = 0, dice;
 		String choice;
 		Random r = new Random();
-		dice = 1 + r.nextInt(6);
-		System.out.println("Player_1 rolls a dice of " + dice);
-		String option[] = { "No_Play", "Ladder", "Snake" };
-		choice = option[r.nextInt(3)];
-		System.out.println(choice);
+		while (true) {
+			dice = 1 + r.nextInt(6);
+			System.out.println("Player_1 rolls a dice of " + dice);
+			String option[] = { "No_Play", "Ladder", "Snake" };
+			choice = option[r.nextInt(3)];
+			System.out.println(choice);
 
-		if (choice == ("Ladder")) {
-			
-			player_1 = player_1 + dice;
-			
-			System.out.println(player_1);
-
-		} else if (choice == ("Snake")) {
-
-			player_1 = player_1 - dice;
-			
-			System.out.println(player_1);
-			
-		} else {
-			System.out.println("No_Play condition occures");
+			if (choice == ("Ladder")) {
+				player_1 = player_1 + dice;
+				System.out.println(player_1);
+			} 
+			else if (choice == ("Snake")) {
+				player_1 = player_1 - dice;
+						if (player_1 < 0) {
+					player_1 = 0;
+				}
+				System.out.println(player_1);
+			} 
+			else {
+				System.out.println("No_Play condition occures");
+			}
+			if (player_1 >= 100)
+			{
+				System.out.println("game ended");
+			break;	
+			}
 		}
-
-		{
-		}
-
 	}
-
 }
