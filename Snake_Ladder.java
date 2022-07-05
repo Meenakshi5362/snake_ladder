3b8448cceb3ae6122de5ba56a6e5046516c5d22a
unknown <annapureddy738@gmail.com>
2022-07-05 15:34:27 +0530

    player rolls a dice

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Snake_Ladder{
 	public static void main(String[] args) {
-		int player_1=0;
+		int player_1=0,dice;
+		Random r = new Random();
+		dice=1+r.nextInt(6);
+		System.out.println("Player_1 rolls a dice of "+dice);
 	}
 }
