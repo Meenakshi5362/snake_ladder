de8ace3d86c32fac66d3c71baaa59583e21cd493
unknown <annapureddy738@gmail.com>
2022-07-05 16:31:06 +0530

    player checks for option noplay,ladder or snake

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,10 +1,35 @@
 import java.util.*;
 
-public class Snake_Ladder{
+public class Snake_Ladder {
 	public static void main(String[] args) {
-		int player_1=0,dice;
+		int player_1 = 0, dice;
+		String choice;
 		Random r = new Random();
-		dice=1+r.nextInt(6);
-		System.out.println("Player_1 rolls a dice of "+dice);
+		dice = 1 + r.nextInt(6);
+		System.out.println("Player_1 rolls a dice of " + dice);
+		String option[] = { "No_Play", "Ladder", "Snake" };
+		choice = option[r.nextInt(3)];
+		System.out.println(choice);
+
+		if (choice == ("Ladder")) {
+			
+			player_1 = player_1 + dice;
+			
+			System.out.println(player_1);
+
+		} else if (choice == ("Snake")) {
+
+			player_1 = player_1 - dice;
+			
+			System.out.println(player_1);
+			
+		} else {
+			System.out.println("No_Play condition occures");
+		}
+
+		{
+		}
+
 	}
+
 }
